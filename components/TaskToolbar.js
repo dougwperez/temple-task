9a318ea5203dfb05e0699191f1b70eb74bf969b5
Doug Perez <perezcpt@gmail.com>
2022-08-16 15:59:44 -0700

    wip

--- components/TaskToolbar.js
+++ components/TaskToolbar.js
@@ -1,12 +1,56 @@
 import React from 'react';
 import {Text, View} from 'react-native';
-import {Button, AppBar} from '@react-native-material/core';
+import {Button, AppBar, HStack, IconButton} from '@react-native-material/core';
+import IconIon from 'react-native-vector-icons/Ionicons';
+import IconFont from 'react-native-vector-icons/FontAwesome5';
 
 const TaskToolbar = () => {
   return (
     <AppBar
+      title="Coins: 2"
+      leading={props => <IconFont name="coins" {...props} />}
       variant="bottom"
-      style={{position: 'absolute', start: 0, end: 0, bottom: 0, height: 60}}
+      style={{
+        position: 'absolute',
+        start: 0,
+        end: 0,
+        bottom: 0,
+        height: 75,
+        paddingHorizontal: 13,
+        paddingTop: 10,
+      }}
+      trailing={props => (
+        <HStack spacing={8}>
+          <Button
+            // variant="outlined"
+            title="Goals"
+            color="white"
+            tintColor="#EEBC1D"
+            backgroundColor="white"
+            leading={props => <IconIon name="checkmark-circle" {...props} />}
+          />
+          <Button
+            // variant="outlined"
+            title="Done!"
+            color="white"
+            tintColor="green"
+            backgroundColor="white"
+            leading={props => <IconIon name="checkmark-circle" {...props} />}
+          />
+          {/* <IconButton
+            icon={props => <IconFoundation name="paint-bucket" {...props} />}
+            {...props}
+          />
+          <IconButton
+            icon={props => <IconMaterial name="toy-brick" {...props} />}
+            {...props}
+          />
+          <IconButton
+            icon={props => <IconMaterial name="delete" {...props} />}
+            {...props}
+          /> */}
+        </HStack>
+      )}
     />
   );
 };
